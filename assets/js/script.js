05cf0aa2ceddfcbf6d1edad0a98dcc8326a42a41
abmetheny <abmetheny@gmail.com>
2023-02-09 18:18:57 -0500

    Search fields and outputs are now cleared when a new search is performed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,38 @@
 var currentWeather = document.getElementById('current-weather-container');
+var currentCity = document.getElementById('city-name');
 var currentHeader = document.getElementById('city-date');
 var searchButton = document.getElementById('search-button');
+var forecastHeader = document.getElementById('forecast-header');
 var forecastWeather = document.getElementById('daily-forecast-container');
 
 var searchInputVal = "";
+
+// function checkInputs() {
+//     if (currentHeader == "") {
+//         getLocation();
+//     } else {
+//         clearInput();
+//         removeNodes();
+//         getLocation();
+//     }
+// }
+
+// function clearInput() {
+//     currentCity.value = '';
+// }
+
+// function removeNodes() {
+//     currentWeather.innerHTML = '';
+//     currentHeader.innerHTML = '';
+//     forecastHeader.innerHTML = '';
+//     forecastWeather.innerHTML = '';
+// }
 
 function getLocation() {
     searchInputVal = document.querySelector('#city-name').value.replace(/\s+/g, '');
     console.log(searchInputVal);
     var requestURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + searchInputVal + '&APPID=c4775d1a77795c9e3426b0f8b3ca1221';
+
 
     fetch(requestURL)
         .then(function(response) {
@@ -25,6 +49,21 @@ function getLocation() {
             getForecast();
             
         })
+
+    function clearInput() {
+        currentCity.value = '';
+    }
+
+    function removeNodes() {
+        currentWeather.innerHTML = '';
+        currentHeader.innerHTML = '';
+        forecastHeader.innerHTML = '';
+        forecastWeather.innerHTML = '';
+    }
+
+    clearInput();
+    removeNodes();
+
 }
 
 function getWeather() {
@@ -74,7 +113,7 @@ function getForecast() {
             console.log(data);
 
             for (var i = 0; i < 5; i++) {
-                
+
                 var div = document.createElement('div');
                 div.className = "col border";
                 var date = document.createElement('li');
@@ -83,6 +122,7 @@ function getForecast() {
                 var wind = document.createElement('li');
                 var humidity = document.createElement('li');
 
+                forecastHeader.innerHTML = '5-Day Forecast:';
                 date.innerHTML = data[i].dt_txt;
                 icon.src = 'http://openweathermap.org/img/wn/' + data[i].weather[0].icon + '@2x.png';
                 temp.innerHTML = 'Temp: ' + data[i].main.temp + '\u00B0F';
